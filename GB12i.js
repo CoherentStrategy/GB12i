7566b6437402f55f12d6d3e5389cd9ed67a8a753
Coherent_Strategy <152778856+CoherentStrategy@users.noreply.github.com>
2026-01-06 17:18:15 +0800

    Add files via upload

---
+++ GB12i.js
@@ -0,0 +1,51 @@
+/* ---------- HELPERS ---------- */
+const $ = id => document.getElementById(id);
+/* ---------- POPUPS ---------- */
+let popupOpen1=false,popupOpen2=false
+
+function openPopup1(html){ $("popupContent1").innerHTML=html; $("popupOverlay1").classList.remove("hidden"); $("popupOverlay1").classList.add("is-visible"); }
+function closePopup1(){ $("popupOverlay1").classList.remove("is-visible"); $("popupOverlay1").classList.add("hidden"); }
+
+function openPopup2(html){ $("popupContent2").innerHTML=html; $("popupOverlay2").classList.remove("hidden"); $("popupOverlay2").classList.add("is-visible"); }
+function closePopup2(){ $("popupOverlay2").classList.remove("is-visible"); $("popupOverlay2").classList.add("hidden"); }
+
+const backdrop = document.getElementById("ad-backdrop");
+
+function openAd() {
+    backdrop.classList.add("is-visible");
+    backdrop.hidden = false;
+}
+
+
+/* ---------- ----------*/
+const register = document.getElementsByClassName("register");
+const login = document.getElementsByClassName("login");
+
+$('registerBtn').addEventListener('click', () => {
+    $('container').classList.remove("active");
+    register.inert = false; // makes login useless
+    login.inert = true;
+});
+
+$('loginBtn').addEventListener('click', () => {
+    $('container').classList.add("active");
+    register.inert = true; // makes register useless
+    login.inert = false;
+});
+
+$("forgotPassword").addEventListener("click", (e) => {
+    e.preventDefault(); // stop page jump
+    forgotPassword();
+});
+
+function forgotPassword(){
+    openAd();
+};
+
+window.addEventListener("message", (e) => {
+    if (e.data?.type === "close-ad") {
+        setTimeout(() => ad.remove(), 300);
+        backdrop.classList.remove("is-visible");
+        backdrop.hidden = true;
+    }
+});
